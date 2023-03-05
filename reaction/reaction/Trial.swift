9269df0f004a2a2eeaad863cbaae7aed8c4b2def
Arman Gorji <gorjiarman@gmail.com>
2023-03-05 18:35:36 +0330

    Add files via upload

---
+++ reaction/reaction/Trial.swift
@@ -0,0 +1,17 @@
+//
+//  Trial.swift
+//  gameon
+//
+//  Created by Arman on 4/12/1401 AP.
+//
+
+import Foundation
+
+class Trial
+{
+    var firstFlashInteral: Double?
+    var secondFlashInteral: Double?
+    var answer: Bool?
+    var reactionTime: Double?
+}
+
